236174385213a4771535efe18c9a60b83dc68e84
Nick Barnwell <nb@ul.io>
2013-11-08 18:12:09 -0800

    Remove unnecessary copyright header

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,9 +1,5 @@
 # Cookbook Name:: wlux_dev
 # Recipe:: default
-#
-# Copyright (C) 2013 YOUR_NAME
-# 
-# All rights reserved - Do Not Redistribute
 
 file_cache_root = Chef::Config[:file_cache_path]
 lampp_dir = "#{node[:xampp][:dir]}/lampp/htdocs"
